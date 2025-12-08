88c2a03499f96a8facd1d46cdc60b4893b1472f1
Mélanie Couronne <melanie.couronne@gmail.com>
2025-12-08 15:14:52 +0100

    Si login ou register on cache le layout de app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./component/common-parts/header/header";
-import { MenuComponent } from "./component/common-parts/menu/menu";
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './component/common-parts/header/header';
+import { MenuComponent } from './component/common-parts/menu/menu';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,12 @@ import { MenuComponent } from "./component/common-parts/menu/menu";
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  router = inject(Router);
+
+  hideLayout(): boolean {
+    const url = this.router.url;
+
+    return url.startsWith('/auth');
+  }
+}
