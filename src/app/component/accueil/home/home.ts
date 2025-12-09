164a404fd7c8a750acd3d69c2479a49a37442b70
Mélanie Couronne <melanie.couronne@gmail.com>
2025-12-09 12:21:48 +0100

    Modif de la home, du menu et du header

--- src/app/component/accueil/home/home.ts
+++ src/app/component/accueil/home/home.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../service/auth';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class HomeComponent {
+  auth = inject(AuthService);
 
+  get isLogged() {
+    return this.auth.currentUser !== null;
+  }
+
+  get user() {
+    return this.auth.currentUser;
+  }
 }
