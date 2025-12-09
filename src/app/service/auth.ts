ea5acc358016c13e2d91e304e3522f54e705c676
Helene <helenebrie@yahoo.fr>
2025-12-09 12:25:15 +0100

    observable connected

--- src/app/service/auth.ts
+++ src/app/service/auth.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { Utilisateur } from '../models/utilisateur';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { NotExpr } from '@angular/compiler';
 
 
 @Injectable({
@@ -9,16 +11,21 @@ import { Utilisateur } from '../models/utilisateur';
 })
 export class AuthService {
   private apiUrl: string;
+  private isLoggedInSubject = new BehaviorSubject<boolean>(this.currentUser != null);
+
+  isConnected$: Observable<boolean> = this.isLoggedInSubject.asObservable();
 
   constructor(private http: HttpClient) {
     this.apiUrl = environment.BACKEND_URL + '/auth';
   }
 
   login(credentials: { email: string; password: string }) {
+    this.isLoggedInSubject.next(true);
     return this.http.post<any>(`${this.apiUrl}/login`, credentials);
   }
 
   logout() {
+    this.isLoggedInSubject.next(false);
     localStorage.removeItem('user');
     localStorage.removeItem('token');
   }
@@ -33,4 +40,9 @@ export class AuthService {
     const data = localStorage.getItem('user');
     return data ? JSON.parse(data) : null;
   }
+
+  isLoggedIn(): boolean {
+    return this.isLoggedInSubject.value;
+  }
+
 }
